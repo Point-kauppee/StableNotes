77cb1b1d71210871384aa5cca4173ee651896b16
Point-kauppee <eero.kauppi@stud.pointcollege.fi>
2017-03-15 12:02:09 +0200

    Add project files.

---
+++ StableNotes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StableNotes.Startup))]
+namespace StableNotes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
